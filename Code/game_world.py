89cc2396de42e5a51cc6961a13e4d1c4afebc217
Vallhup <105789043+Vallhup@users.noreply.github.com>
2022-11-05 20:34:26 +0900

    게임 월드 추가

---
+++ Code/game_world.py
@@ -0,0 +1,31 @@
+# game world
+
+# layer 0 : Background Objects
+# layer 1 : Foreground objects
+world = [[], []]
+
+def add_object(o, depth):
+    world[depth].append(o)
+
+def add_objects(ol, depth):
+    world[depth] += ol
+
+def remove_object(o):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            del o
+            return
+    raise ValueError('Trying destroy non existing object')
+
+def all_objects():
+    for layer in world:
+        for o in layer:
+            yield o
+
+def clear():
+    for o in all_objects():
+        del o
+
+    for layer in world:
+        layer.clear()

aae1334441571e49d7dfca307ed96e4a84015efd
Vallhup <105789043+Vallhup@users.noreply.github.com>
2022-11-12 20:30:54 +0900

    충돌 그룹 추가, 삭제 함수 추가

--- Code/game_world.py
+++ Code/game_world.py
@@ -4,6 +4,8 @@
 # layer 1 : Foreground objects
 world = [[], []]
 
+collision_group = dict()
+
 def add_object(o, depth):
     world[depth].append(o)
 
@@ -14,6 +16,7 @@ def remove_object(o):
     for layer in world:
         if o in layer:
             layer.remove(o)
+            remove_collision_object(o)
             del o
             return
     raise ValueError('Trying destroy non existing object')
@@ -29,3 +32,36 @@ def clear():
 
     for layer in world:
         layer.clear()
+
+def add_collision_group(a, b, group):
+    if group not in collision_group:
+        print('New Group Made')
+        collision_group[group] = [ [], [] ]
+
+    if a:
+        if type(a) == list:
+            collision_group[group][0] += a
+
+        else:
+            collision_group[group][0].append(a)
+
+    if b:
+        if type(b) == list:
+            collision_group[group][1] += b
+
+        else:
+            collision_group[group][1].append(b)
+
+
+def all_collision_pairs():
+    # collision group이라는 딕셔너리에서, 각 리스트로부터 페어를 만들어서 보내준다.
+    for group, pairs in collision_group.items(): # items() key, value
+        for a in pairs[0]:
+            for b in pairs[1]:
+                yield a, b, group
+
+
+def remove_collision_object(o):
+    for pairs in collision_group.values(): # key:value에서 value에 해당되는 것만 가져온다.
+        if o in pairs[0]: pairs[0].remove(o)
+        if o in pairs[1]: pairs[1].remove(o)
